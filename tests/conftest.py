237074b618833d7cf95f5863448e7296274ef25c
Joyce Byun <joyce.byun@protonmail.com>
2023-03-15 14:52:29 -0700

    Move pytest fixture to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,33 @@
+import pytest
 import os
 
 # Force geopandas to use Shapely 2.0 instead of PyGEOS
 # (PyGEOS was merged with Shapely, and will stop working in a future release of GeoPandas)
 os.environ['USE_PYGEOS'] = '0'
+
+import osmnx as ox
+
+
+@pytest.fixture
+def G():
+    address = "501 E 9th St, Tucson, AZ 85705"
+
+    MDG = ox.graph.graph_from_address(address=address,
+                                      dist=1000,  # meters from center
+                                      dist_type='bbox',
+                                      network_type='drive',
+                                      simplify=False,
+                                      retain_all=True,
+                                      truncate_by_edge=False,
+                                      return_coords=False,
+                                      clean_periphery=False,
+                                      custom_filter=None)
+
+    # Remove interstitial nodes (nodes that are not intersections or dead-ends)
+    MDG = ox.simplification.simplify_graph(MDG,
+                                           strict=False,
+                                           remove_rings=False,
+                                           track_merged=False)
+
+    MG = ox.utils_graph.get_undirected(MDG)
+    return MG
--- tests/test_route.py
+++ tests/test_route.py
@@ -1,31 +1,4 @@
-import pytest
-import osmnx as ox
 import route
-
-
-@pytest.fixture
-def G():
-    address = "501 E 9th St, Tucson, AZ 85705"
-
-    MDG = ox.graph.graph_from_address(address=address,
-                                      dist=1000,  # meters from center
-                                      dist_type='bbox',
-                                      network_type='drive',
-                                      simplify=False,
-                                      retain_all=True,
-                                      truncate_by_edge=False,
-                                      return_coords=False,
-                                      clean_periphery=False,
-                                      custom_filter=None)
-
-    # Remove interstitial nodes (nodes that are not intersections or dead-ends)
-    MDG = ox.simplification.simplify_graph(MDG,
-                                           strict=False,
-                                           remove_rings=False,
-                                           track_merged=False)
-
-    MG = ox.utils_graph.get_undirected(MDG)
-    return MG
 
 
 def test_route(G):
