42e3b6e671aafabe8c9a6490feb7dd1f21bd999c
Joyce Byun <joyce.byun@protonmail.com>
2023-03-15 16:53:04 -0700

    Add get_incident_edges to RouteFinder

--- src/route_finder.py
+++ src/route_finder.py
@@ -1,4 +1,5 @@
 import copy
+from edge import Edge
 
 
 class RouteFinder():
@@ -6,4 +7,8 @@ class RouteFinder():
         self.G = copy.deepcopy(G)
         self.source: int = source
         self.max_distance: float = max_distance
+
+    def get_incident_edges(self, u):
+        # get list of edges incident on u, where each edge is a tuple (u,v,k,length)
+        return [Edge(*e) for e in self.G.edges(nbunch=u, data='length', keys=True)]
 
--- tests/test_route_finder.py
+++ tests/test_route_finder.py
@@ -1,16 +1,37 @@
 import pytest
 import osmnx as ox
 from route_finder import RouteFinder
+from edge import Edge
 
 
 @pytest.fixture
-def rf(G, center_point):
-    source = ox.distance.nearest_nodes(G, *center_point, return_dist=False)
+def source(G, center_point):
+    return ox.distance.nearest_nodes(G, *center_point, return_dist=False)
+
+
+@pytest.fixture
+def rf(G, source):
     max_distance = 500.0
     return RouteFinder(G, source, max_distance)
 
 
-def test_route_finder_init(G, rf):
+def test_route_finder_init(G, rf, source):
     assert rf.source in G.nodes
-    assert rf.source == 7477543909
+    assert rf.source == source
     assert rf.max_distance == pytest.approx(500.0)
+
+
+def test_get_incident_edges(G, rf, source):
+    n = source
+    incident_edges = rf.get_incident_edges(n)
+
+    # check that incident_edges does not include any non-incident edges
+    for e in incident_edges:
+        assert e.u == n or e.v == n
+
+    # check that we are not missing any incident edges
+    all_edges = [Edge(*i) for i in G.edges(data='length', keys=True)]
+    for e in all_edges:
+        if (e.u == n or e.v == n):
+            flipped = Edge(u=e.v, v=e.u, key=e.key, length=e.length)
+            assert e in incident_edges or flipped in incident_edges
