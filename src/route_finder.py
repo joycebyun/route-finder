af2586d7d49f93eda0d9b76f7319d4d6ec6b034b
Joyce Byun <joyce.byun@protonmail.com>
2023-03-15 17:40:37 -0700

    Add get_viable_edges to RouteFinder class

--- src/route_finder.py
+++ src/route_finder.py
@@ -1,6 +1,7 @@
 import copy
 from edge import Edge
-
+from route import Route
+import networkx as nx
 
 class RouteFinder():
     def __init__(self, G, source, max_distance):
@@ -12,3 +13,20 @@ class RouteFinder():
         # get list of edges incident on u, where each edge is a tuple (u,v,k,length)
         return [Edge(*e) for e in self.G.edges(nbunch=u, data='length', keys=True)]
 
+    def get_viable_edges(self, route: Route):
+        # a viable edge is an incident edge that we could traverse, 
+        # and still get back to source with distance <= max_distance
+
+        u = route.nodes[-1]
+        incident_edges = self.get_incident_edges(u)
+
+        viable = []
+        for e in incident_edges:
+            if (route.distance + e.length
+                + nx.shortest_path_length(self.G,
+                                          source=e.v,
+                                          target=self.source,
+                                          weight='length')) <= self.max_distance:
+                viable.append(e)
+
+        return viable
--- tests/test_route_finder.py
+++ tests/test_route_finder.py
@@ -1,7 +1,15 @@
 import pytest
-import osmnx as ox
+
+import os
+# Force geopandas to use Shapely 2.0 instead of PyGEOS
+# (PyGEOS was merged with Shapely, and will stop working in a future release of GeoPandas)
+os.environ['USE_PYGEOS'] = '0'
+import osmnx as ox 
+
 from route_finder import RouteFinder
-from edge import Edge
+from edge import Edge, flip
+from route import Route
+import networkx as nx
 
 
 @pytest.fixture
@@ -15,23 +23,56 @@ def rf(G, source):
     return RouteFinder(G, source, max_distance)
 
 
-def test_route_finder_init(G, rf, source):
-    assert rf.source in G.nodes
+def test_route_finder_init(rf, source):
+    assert rf.source in rf.G.nodes
     assert rf.source == source
     assert rf.max_distance == pytest.approx(500.0)
 
 
-def test_get_incident_edges(G, rf, source):
+def test_get_incident_edges(rf, source):
     n = source
     incident_edges = rf.get_incident_edges(n)
 
     # check that incident_edges does not include any non-incident edges
     for e in incident_edges:
-        assert e.u == n or e.v == n
+        assert e.u == n
 
     # check that we are not missing any incident edges
-    all_edges = [Edge(*i) for i in G.edges(data='length', keys=True)]
+    all_edges = [Edge(*i) for i in rf.G.edges(data='length', keys=True)]
     for e in all_edges:
-        if (e.u == n or e.v == n):
-            flipped = Edge(u=e.v, v=e.u, key=e.key, length=e.length)
-            assert e in incident_edges or flipped in incident_edges
+        if e.u == n:
+            assert e in incident_edges
+        elif e.v == n:
+            assert flip(e) in incident_edges
+
+
+@pytest.fixture
+def route_fixture(rf, source):
+    r = Route(rf.G)
+    r.nodes.append(source)
+    return r
+
+
+def test_get_viable_edges(rf, route_fixture):
+    viable_edges = rf.get_viable_edges(route_fixture)
+
+    # check that the edges are viable
+    for e in viable_edges:
+        assert (route_fixture.distance
+                + e.length
+                + nx.shortest_path_length(rf.G,
+                                          source=e.v,
+                                          target=rf.source,
+                                          weight='length')) <= rf.max_distance
+
+    # check that we are not missing any viable edges
+    u = route_fixture.nodes[-1]
+    incident_edges = rf.get_incident_edges(u)
+    for e in incident_edges:
+        if e not in viable_edges:
+            assert (route_fixture.distance
+                    + e.length
+                    + nx.shortest_path_length(rf.G,
+                                              source=e.v,
+                                              target=rf.source,
+                                              weight='length')) > rf.max_distance
