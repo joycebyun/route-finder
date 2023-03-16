eb89a41cf0da802f2ddc34a78edf00758e5775d7
Joyce Byun <joyce.byun@protonmail.com>
2023-03-15 17:58:36 -0700

    Add type hints to Route class

--- src/route.py
+++ src/route.py
@@ -1,15 +1,18 @@
 import copy
-import networkx as nx
+import networkx as nx  # type: ignore
+from typing import List
+from edge import Edge
 
 
 class Route():
-    def __init__(self, G):
+    def __init__(self, G) -> None:
 
         self.G = copy.deepcopy(G)
 
         # set all nodes to have attribute 'visited' = False
         nx.set_node_attributes(self.G, name='visited', values=False)
 
-        self.nodes = []
-        self.edges = []
-        self.distance = 0
+        self.nodes: List[int] = []
+        self.edges: List[Edge] = []
+        self.distance: float = 0
+
